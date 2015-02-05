0b339f73e9c71c44082f4241eed2ac5641fef351
Mark E. DeYoung <mark.e.deyoung@gmail.com>
2015-02-05 08:42:28 -0500

    another fix_name.ps1 fix

--- fix_name.ps1
+++ fix_name.ps1
@@ -2,6 +2,6 @@
 #See: http://blogs.technet.com/b/heyscriptingguy/archive/2008/01/17/how-can-i-use-windows-powershell-to-replace-characters-in-a-text-file.aspx
  
 param([string]$name = $(throw "Use the -name parameter to specify your project's name."))
-gci . -recurse | ForEach {
-  (Get-Content $_ | ForEach {$_ -replace "\{\{ project_name \}\}", $name}) | Set-Content $_ 
+gci . | ForEach {
+  (Get-Content $_ )| ForEach {$_ -replace "\{\{ project_name \}\}", $name} | Set-Content $_ 
 }
